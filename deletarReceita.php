f91ecf44cd79dfa627c592519b2426e1cd79c546
Luan Lobo <luanfamilia2015@gmail.com>
2024-02-08 16:28:59 -0300

    ADICIONEI O DELETE EM DESPESA, E EDITAR E CONFIRMAR EDIÇÃO

--- deletarReceita.php
+++ deletarReceita.php
@@ -3,7 +3,7 @@ require "./config.php";
 
 $id = $_GET["id"];
 
-$sql = "DELETE FROM receitas WHERE id = :id";
+$sql = "DELETE FROM receita WHERE id = :id";
 $sql = $pdo->prepare($sql);
 
 $sql->bindValue(":id", $id);
---
+++ editarDespesa.php
@@ -0,0 +1,71 @@
+<?php
+require "./config.php";
+
+$id = $_GET["id"];
+
+$sql = "SELECT * FROM despesa WHERE id = :id";
+$sql = $pdo->prepare($sql);
+$sql->bindValue(":id", $id);
+
+$sql->execute();
+
+$dado = $sql->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/style.css">
+  <title>Receitas</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="mostrarCategoria.php">Categorias</a></li>
+        <li><a href="receitas.php">Receitas</a></li>
+        <li><a href="despesa.php">Despesas</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <form action="confirmarEdicaoReceita.php" method="get">
+      <input type="hidden" name="id" value="<?= $dado['id']; ?>">
+      <label>
+        Descrição
+        <input type="text" name="descricao" value="<?= $dado['descricao']; ?>" required>
+      </label>
+
+      <label>
+        Valor
+        <input type="number" name="valor" value="<?= $dado['valor']; ?>" required>
+      </label>
+
+      <label>
+        Categoria
+        <select name="categoria" value="Bonus">
+          <option value="despesa">Despesa</option>
+          <option value="receita">Receita</option>
+        </select>
+      </label>
+
+      <label>
+        Data
+        <input type="date" name="data_mvto" value="<?= $dado['data_mvto'] ?>" required>
+      </label>
+
+      <button type="submit">Editar</button>
+
+    </form>
+
+
+  </main>
+  <script src="https://kit.fontawesome.com/561265e797.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
