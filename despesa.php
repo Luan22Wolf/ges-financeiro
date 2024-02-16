297b98202fb2e0ef924027ce6f3a0ccbb64f06a1
Luan Lobo <luanfamilia2015@gmail.com>
2024-02-16 16:04:41 -0300

    NOVAS ALTERAÇÕES

--- despesa.php
+++ despesa.php
@@ -5,7 +5,6 @@ $sql = "SELECT * FROM despesa";
 $sql = $pdo->prepare($sql);
 $sql->execute();
 
-
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 $sql = "SELECT SUM(valor) as valor FROM despesa";
@@ -19,85 +18,88 @@ $total_receita = $sql->fetch(PDO::FETCH_ASSOC);
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./styles/style.css">
-  <title>Receitas</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Receitas</title>
 </head>
 
 <body>
-  <header>
+<header>
     <nav>
-      <ul>
-        <li><a href="mostrarCategoria.php">categoria</a></li>
-        <li><a href="receitas.php">receita</a></li>
-        <li><a href="despesa.php">despesa</a></li>
-      </ul>
+        <ul>
+            <li><a href="mostrarCategoria.php">categoria</a></li>
+            <li><a href="receitas.php">receita</a></li>
+            <li><a href="despesa.php">despesa</a></li>
+        </ul>
     </nav>
-  </header>
+</header>
 
-  <main>
+<main>
     <span class="total">
       <p>Total de Receita: R$<?= $total_receita["valor"]?></p>
     </span>
     <form action="cadastrarDespesa.php" method="get">
-      <label>
-        Descrição
-        <input type="text" name="descricao" required>
-      </label>
+        <label>
+            Descrição
+            <input type="text" name="descricao" required>
+        </label>
 
-      <label>
-        Valor
-        <input type="number" name="valor" required>
-      </label>
+        <label>
+            Valor
+            <input type="number" name="valor" required>
+        </label>
 
-      <label>
-        Categoria
-        <select name="categoria">
-          <option value="despesa">Despesa</option>
-          <option value="receita">Receita</option>
-        </select>
-      </label>
+        <label>
+            Categoria
+            <select name="categoria">
+                <option value="despesa">Despesa</option>
+                <option value="receita">Receita</option>
+            </select>
+        </label>
 
-      <label>
-        Data
-        <input type="date" name="data_mvto" required>
-      </label>
+        <label>
+            Data
+            <input type="date" name="data_mvto" required>
+        </label>
 
-      <button type="submit">Adicionar</button>
+        <button type="submit">Adicionar</button>
 
     </form>
 
     <table>
-      <thead>
+        <thead>
         <tr>
-          <th>ID</th>
-          <th>Descrição &#x1F4DD;</th>
-          <th>Valor &#x1F4B2;</th>
-          <th>Data &#x1F4C5;</th>
-          <th>Categoria &#x1F354;</th>
-          <th>Opções &#x2699;</th>
+            <th>ID</th>
+            <th>Descrição &#x1F4DD;</th>
+            <th>Valor &#x1F4B2;</th>
+            <th>Data &#x1F4C5;</th>
+            <th>Categoria &#x1F354;</th>
+            <th>Opções &#x2699;</th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         <?php foreach ($dados as $dado) : ?>
-          <tr>
-            <td><?= $dado['id'] ?></td>
-            <td><?= $dado['descricao'] ?></td>
-            <td><?= $dado['valor'] ?></td>
-            <td><?= $dado['data_mvto'] ?></td>
-            <td><?= $dado['categoria_id'] ?></td>
-            <td>
-              <a href="./deletarDespesa.php?id=<?= $dado['id'] ?>"><i class="btn-deletar fa-solid fa-trash"></i></a>
-              <a href="./editarDespesa.php?id=<?= $dado['id'] ?>" class="btn-editar"><i class="fa-solid fa-pen-to-square"></i></a>
-            </td>
-          </tr>
+            <tr>
+                <td><?= $dado['id'] ?></td>
+                <td><?= $dado['descricao'] ?></td>
+                <td><?= $dado['valor'] ?></td>
+                <td><?= $dado['data_mvto'] ?></td>
+                <td><?= $dado['categoria_id'] ?></td>
+                <td>
+                    <!-- Adicionando a confirmação -->
+                    <a href="./deletarDespesa.php?id=<?= $dado['id'] ?>" onclick="return confirm('Tem certeza que deseja excluir esta despesa?');">
+                        <i class="btn-deletar fa-solid fa-trash"></i>
+                    </a>
+                    <a href="./editarDespesa.php?id=<?= $dado['id'] ?>" class="btn-editar"><i class="fa-solid fa-pen-to-square"></i></a>
+                </td>
+            </tr>
         <?php endforeach; ?>
-      </tbody>
+        </tbody>
     </table>
 
-  </main>
-  <script src="https://kit.fontawesome.com/561265e797.js" crossorigin="anonymous"></script>
+</main>
+<script src="https://kit.fontawesome.com/561265e797.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- receitas.php
+++ receitas.php
@@ -5,7 +5,6 @@ $sql = "SELECT * FROM receita";
 $sql = $pdo->prepare($sql);
 $sql->execute();
 
-
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 $sql = "SELECT SUM(valor) as valor FROM receita";
@@ -19,88 +18,91 @@ $total_receita = $sql->fetch(PDO::FETCH_ASSOC);
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./styles/style.css">
-  <title>Gerenciador Financeiro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Gerenciador Financeiro</title>
 </head>
 
 <body>
-  <header>
+<header>
     <nav>
-      <ul>
-        <li><a href="mostrarCategoria.php">categoria</a></li>
-        <li><a href="receitas.php">receita</a></li>
-        <li><a href="despesa.php">despesa</a></li>
-      </ul>
+        <ul>
+            <li><a href="mostrarCategoria.php">categoria</a></li>
+            <li><a href="receitas.php">receita</a></li>
+            <li><a href="despesa.php">despesa</a></li>
+        </ul>
     </nav>
-  </header>
+</header>
 
-  <main>
+<main>
     <span class="total">
       <p>Total de Receita: R$<?= $total_receita["valor"]?></p>
     </span>
     <form action="cadastrarReceita.php" method="get">
 
-      <label>
-        Descrição
-        <input type="text" name="descricao" required>
-      </label>
+        <label>
+            Descrição
+            <input type="text" name="descricao" required>
+        </label>
 
-      <label>
-        Valor
-        <input type="number" name="valor" required>
-      </label>
+        <label>
+            Valor
+            <input type="number" name="valor" required>
+        </label>
 
-      <label>
-        Categoria
-        <select name="categoria">
-          <option value="despesa">Despesa</option>
-          
-        </select>
-      </label>
+        <label>
+            Categoria
+            <select name="categoria">
+                <option value="despesa">Despesa</option>
 
-      <label>
-        Data
-        <input type="date" name="data_mvto" required>
-      </label>
+            </select>
+        </label>
 
-      <button type="submit">Adicionar</button>
+        <label>
+            Data
+            <input type="date" name="data_mvto" required>
+        </label>
+
+        <button type="submit">Adicionar</button>
 
     </form>
 
     <table>
-      <thead>
+        <thead>
         <tr>
-          <th>ID</th>
-          <th>Descrição &#x1F4DD;</th>
-          <th>Valor &#x1F4B2;</th>
-          <th>Data &#x1F4C5;</th>
-          <th>Categoria &#x1F354;</th>
-          <th>Opções &#x2699;</th>
+            <th>ID</th>
+            <th>Descrição &#x1F4DD;</th>
+            <th>Valor &#x1F4B2;</th>
+            <th>Data &#x1F4C5;</th>
+            <th>Categoria &#x1F354;</th>
+            <th>Opções &#x2699;</th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         <?php foreach ($dados as $dado) : ?>
-          <tr>
-            <td><?= $dado['id'] ?></td>
-            <td><?= $dado['descricao'] ?></td>
-            <td><?= $dado['valor'] ?></td>
-            <td><?= $dado['data_mvto'] ?></td>
-            <td><?= $dado['categoria_id'] ?></td>
-            <td>
-              <a href="./deletarReceita.php?id=<?= $dado['id'] ?>"><i class="btn-deletar fa-solid fa-trash"></i></a>
-              <a href="./editarReceita.php?id=<?= $dado['id'] ?>" class="btn-editar"><i class="fa-solid fa-pen-to-square"></i></a>
-              <a href="./status_pago.php?id=<?= $dado['id'] ?>" class="check">&#x2713;</a>
-              <a href="./status_pago.php?id=<?= $dado['id'] ?>" class="x">&#x2717;</a>
-            </td>
-          </tr>
+            <tr>
+                <td><?= $dado['id'] ?></td>
+                <td><?= $dado['descricao'] ?></td>
+                <td><?= $dado['valor'] ?></td>
+                <td><?= $dado['data_mvto'] ?></td>
+                <td><?= $dado['categoria_id'] ?></td>
+                <td>
+                    <!-- Adicionando a confirmação -->
+                    <a href="./deletarReceita.php?id=<?= $dado['id'] ?>" onclick="return confirm('Tem certeza que deseja excluir esta receita?');">
+                        <i class="btn-deletar fa-solid fa-trash"></i>
+                    </a>
+                    <a href="./editarReceita.php?id=<?= $dado['id'] ?>" class="btn-editar"><i class="fa-solid fa-pen-to-square"></i></a>
+                    <a href="./status_pago.php?id=<?= $dado['id'] ?>" class="check">&#x2713;</a>
+                    <a href="./status_pago.php?id=<?= $dado['id'] ?>" class="x">&#x2717;</a>
+                </td>
+            </tr>
         <?php endforeach; ?>
-      </tbody>
+        </tbody>
     </table>
 
-  </main>
-  <script src="https://kit.fontawesome.com/561265e797.js" crossorigin="anonymous"></script>
+</main>
+<script src="https://kit.fontawesome.com/561265e797.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
